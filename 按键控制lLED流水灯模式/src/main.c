a9a63d42eaa7384fe2276a0480a4de5ffc550ec8
ZHOUIU <zhoucookie@outlook.com>
2022-01-19 22:58:24 +0800

    按键控制LED流水灯模式&定时器时钟

---
+++ 按键控制lLED流水灯模式/src/main.c
@@ -0,0 +1,48 @@
+#include <REGX52.H>
+#include "Timer0.h"
+#include "Key.h"
+#include <INTRINS.H>
+
+unsigned char KeyNum,LEDMode;
+
+void main()
+{
+    P2=0XFE;
+    Timer0Init();
+    while(1)
+    {
+        KeyNum=Key();
+        if(KeyNum)
+        {
+            if(KeyNum==1)
+            {
+                LEDMode++;
+                if(LEDMode>=2)LEDMode=0;
+            }
+        }
+    }
+}
+
+
+void Timer0_Routine() interrupt 1
+{
+    static unsigned int T0Count;
+    TL0 = 0x18;		//设置定时初值
+ 	TH0 = 0xFC;		//设置定时初值
+    T0Count++;
+
+    if (T0Count>=500)
+    {
+        T0Count=0;
+        if(LEDMode==0)
+        {
+            P2=_crol_(P2,1);
+        }
+        if(LEDMode==1)
+        {
+            P2=_cror_(P2,1);
+        }
+    }
+        
+    
+}
